cada06f4c3feac0f278a1b12aed51b5766f7f986
maxjonas2 <maxjonas2@hotmail.com>
2023-11-09 01:20:07 -0300

    Implemented stack

--- Etapa4/LinkedStack.java
+++ Etapa4/LinkedStack.java
@@ -1,30 +1,51 @@
 package Etapa4;
 
 public class LinkedStack<T> implements Stack<T> {
-    public LinkedStack() {
+
+    public static void main(String[] args) {
+        LinkedStack<String> stringStack = new LinkedStack<String>("Teste");
+        stringStack.push("Nome 1");
+        stringStack.push("Nome 2");
+        stringStack.push("Nome 3");
 
     }
 
-    public T pop() {
-        // TODO Auto-generated method stub
-        return null;
+    public Node<T> head;
+    public int size = 0;
+
+    public LinkedStack(T firstValue) {
+        Node<T> firstNode = new Node<T>(firstValue);
+        this.head = firstNode;
+        this.size++;
     }
 
-    public void push() {
-        // TODO Auto-generated method stub
+    public T pop() throws Exception {
+        if (this.size == 0)
+            throw new Exception("Underflow");
+        T value = this.head.value;
+        this.head = this.head.next;
+        this.size--;
+        return value;
+    }
 
+    public void push(T value) {
+        Node<T> newNode = new Node<T>(value);
+        Node<T> temp = this.head;
+        this.head = newNode;
+        this.head.next = temp;
+        this.size++;
     }
 
     public T top() {
-        // TODO Auto-generated method stub
-        return null;
+        // Check if empty
+        return this.head.value;
     }
 }
 
 interface Stack<T> {
-    public void push();
+    public void push(T value);
 
     public T top();
 
-    public T pop();
+    public T pop() throws Exception;
 }
