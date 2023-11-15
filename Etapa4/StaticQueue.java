28bfc08840da36ee103cd3a8d1e2230ff05d44de
maxjonas2 <maxjonas2@hotmail.com>
2023-11-15 13:45:01 -0300

    Added fix to queue

--- Etapa4/StaticQueue.java
+++ Etapa4/StaticQueue.java
@@ -1,68 +1,120 @@
 package Etapa4;
 
+import java.util.Random;
+
 import exceptions.OverflowException;
 import exceptions.UnderflowException;
 import interfaces.IQueue;
 
 public class StaticQueue<E> implements IQueue<E> {
 
-    protected int first = -1;
-    protected int last = -1;
-    protected int size = 0;
-    protected E[] queue;
+    private int first = -1;
+    private int last = -1;
+    private int maxSize = 0;
+    private E[] queue;
+    private int numElements = 0;
+
+    public static void main(String[] args) throws OverflowException, UnderflowException {
+        Random randGen = new Random();
+
+        StaticQueue<Integer> q = new StaticQueue<Integer>(10);
+
+        for (int i = 0; i < q.maxSize; i++) {
+            q.enqueue(randGen.nextInt(20));
+        }
+
+        q.printQueue();
+
+    }
 
     @SuppressWarnings("unchecked")
-    public StaticQueue(int size) {
-        if (size <= 0)
+    public StaticQueue(int maxSize) {
+        if (maxSize <= 1)
             throw new IllegalArgumentException();
-        this.queue = (E[]) new Object[size];
+        this.maxSize = maxSize;
+        this.queue = (E[]) new Object[maxSize];
     }
 
     public E back() throws UnderflowException {
-        return null;
+        if (this.isEmpty())
+            throw new UnderflowException("Lista está vazia.");
+        return this.queue[last];
     }
 
     public E dequeue() throws UnderflowException {
         if (this.isEmpty())
-            throw new UnderflowException("Lista vazia");
+            throw new UnderflowException("Queue is empty");
 
-        E element = this.queue[first];
-        this.queue[first] = null;
+        E element = this.queue[this.first];
+        this.queue[this.first] = null;
 
-        this.first++;
+        this.numElements--;
 
-        if (this.first == this.queue.length - 1)
+        if (this.numElements == 0) {
+            this.resetIndices();
+        } else if (this.first == this.maxSize - 1) {
             this.first = 0;
+        } else {
+            this.first++;
+        }
 
         return element;
     }
 
     public void enqueue(E element) throws OverflowException {
-        if (this.isFull())
-            throw new OverflowException("Fila cheia.");
+        if (this.isFull()) {
+            throw new OverflowException("Queue is full");
+        } else if (this.isEmpty()) {
+            this.first = 0;
+            this.last = 0;
+            System.out.println("Empty queue. Reset both indices to zero.");
+        } else {
+            if (this.last == this.maxSize - 1) {
+                this.last = 0;
+                System.out.println("Last index at final pos. Reset last index to zero.");
+            } else {
+                System.out.println("Incremented last index");
+                this.last++;
+            }
+        }
 
-        // Check if is end of array
-
-        this.last++;
         this.queue[this.last] = element;
+        this.numElements++;
+        System.out.println("Enqueued new element at position " + this.last);
 
     }
 
     public E front() throws UnderflowException {
-        // Handle circleback case
-        return this.queue[this.first];
+        if (this.isEmpty())
+            throw new UnderflowException("Pilha esta vazia.");
+        return this.queue[first];
     }
 
     public boolean isEmpty() {
-        return this.last == -1 && this.first == -1;
+        return this.numElements == 0;
     }
 
     public boolean isFull() {
-        return Math.abs(this.last) - Math.abs(this.first) == this.size - 1;
+        return this.numElements == this.maxSize;
     }
 
     public int numElements() {
-        return 0;
+        return this.numElements;
+    }
+
+    public void printQueue() {
+        String str = "[";
+        for (E element : this.queue) {
+            str += (element != null ? element.toString() : "null") + ", ";
+        }
+        str = str.substring(0, str.length() - 1);
+        str += "]";
+        System.out.println(str);
+    }
+
+    private void resetIndices() {
+        this.first = -1;
+        this.last = -1;
     }
 
 }
